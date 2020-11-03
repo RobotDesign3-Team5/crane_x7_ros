4045abf005925fec3da1b45f1bc05edd11a945f2
Masato Kubotera <s19c1040hd@s.chibakoudai.jp>
2020-11-03 15:44:45 +0900

    プログラムの整頓とインク確認の動作の見直し

--- crane_x7_examples/scripts/robotdesign3_team5.py
+++ crane_x7_examples/scripts/robotdesign3_team5.py
@@ -11,35 +11,38 @@ from tf.transformations import quaternion_from_euler
 def main():
     # --------------------
     # はんこ
-    seal_x = 0.30
-    seal_y = -0.15
-    seal_before_z = 0.30
-    seal_z = 0.125
-    seal_after_z = 0.30
-    seal_close = 0.22
+    seal_x = 0.30           # x座標[m]
+    seal_y = -0.15          # y座標[m]
+    seal_before_z = 0.30    # 掴む前  Z座標[m]
+    seal_z = 0.125          # 掴む    Z座標[m]
+    seal_after_z = 0.30     # 掴む後  Z座標[m]
+    seal_close = 0.22       # 掴む角度[rad]
     # --------------------
     # 朱肉
-    inkpad_x = 0.20
-    inkpad_y = -0.15
-    inkpad_before_z = 0.30
-    inkpad_z = 0.13
-    inkpad_after_z = 0.30
+    inkpad_x = 0.20         # x座標[m]
+    inkpad_y = -0.15        # y座標[m]
+    inkpad_before_z = 0.30  # 押す前  z座標[m]
+    inkpad_z = 0.13         # 押す    z座標[m]
+    inkpad_after_z = 0.30   # 押す後  z座標[m]
     # --------------------
     # 捺印
-    put_x = 0.20
-    put_y = 0.0
-    put_before_z = 0.20
-    put_z = 0.12
-    put_after_z = 0.20
+    put_x = 0.20            # x座標[m]
+    put_y = 0.0             # y座標[m]
+    put_before_z = 0.20     # 押す前  z座標[m]
+    put_z = 0.12            # 押す    z座標[m]
+    put_after_z = 0.20      # 押す後  z座標[m]
     # --------------------
-    hand_open = math.pi/4
-
+    # 初期設定
+    hand_open = math.pi/4  # ハンド 開く角度[rad] 
+    
+    # --------------------
     rospy.init_node("crane_x7_pick_and_place_controller")
     robot = moveit_commander.RobotCommander()
     arm = moveit_commander.MoveGroupCommander("arm")
-    arm.set_max_velocity_scaling_factor(0.5)
+    arm.set_max_velocity_scaling_factor(0.1) # 実行速度
     gripper = moveit_commander.MoveGroupCommander("gripper")
     # --------------------
+    # 指定座標に手先を動かす関数
     def arm_move(x,y,z):
         target_pose = geometry_msgs.msg.Pose()
         target_pose.position.x = x
@@ -53,18 +56,20 @@ def main():
         arm.set_pose_target(target_pose)
         arm.go()
     # --------------------
+    # ハンドの角度[rad]を指定し動かす関数
     def hand_move(rad):
         gripper.set_joint_value_target([rad, rad])
         gripper.go()
     # --------------------
+    # 指定関節の角度[deg]を指定し動かす関数
     def joint_move(joint_value,deg):
         target_joint_values = arm.get_current_joint_values() # 現在角度をベースに、目標角度を作成する
         target_joint_values[joint_value] = arm.get_current_joint_values()[joint_value] + math.radians(deg)
         arm.set_joint_value_target(target_joint_values)
         arm.go()
     # --------------------
+    # 複数関節の角度[deg]を指定し動かす関数
     def joints_moves(deg):
-        # deg = [deg0,deg1,deg2,deg3,deg4,deg5,deg6]
         target_joint_values = arm.get_current_joint_values() # 現在角度をベースに、目標角度を作成する
         for i in range(7):
             target_joint_values[i] = arm.get_current_joint_values()[i] + math.radians(deg[i])
@@ -74,7 +79,7 @@ def main():
     while len([s for s in rosnode.get_node_names() if 'rviz' in s]) == 0:
         rospy.sleep(1.0)
     rospy.sleep(1.0)
-
+    # --------------------
     print("Group names:")
     print(robot.get_group_names())
 
@@ -82,10 +87,13 @@ def main():
     print(robot.get_current_state())
     # --------------------
     # 簡易SRDFのテスト
+    """
     arm.set_named_target("vertical")
     arm.go()
 
-    joints_moves([-45,-45,-45,-45,-45,-45,-45])
+    joints_moves([41,-3,-81,-99,3,-82,-130])
+    rospy.sleep(1.0)
+    """
     # --------------------
     # アーム初期ポーズを表示
     arm_initial_pose = arm.get_current_pose().pose
@@ -111,18 +119,18 @@ def main():
     print("はんこを持ち上げる")
     arm_move(seal_x, seal_y, seal_after_z)
 
-    print("朱肉上まで移動")
-    arm_move(inkpad_x, inkpad_y, inkpad_before_z)
-
     for i in range(2):
+        print("朱肉上まで移動")
+        arm_move(inkpad_x, inkpad_y, inkpad_before_z)
+
         print("朱肉に押す")
         arm_move(inkpad_x, inkpad_y, inkpad_z)
 
         print("はんこを持ち上げる")
         arm_move(inkpad_x, inkpad_y, inkpad_after_z)
 
-    print("インクが付いたか確認する")
-    joint_move(4,50)
+        print("インクが付いたか確認する")
+        joint_move(4,50)
 
     print("捺印場所に移動")
     arm_move(put_x, put_y, put_before_z)
